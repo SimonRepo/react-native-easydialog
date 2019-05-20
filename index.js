38ecda2865e71e51033062c1dd35e45e6dd37da3
linxiaosheng <linxiaosheng@0easy.com>
2019-05-20 16:02:07 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import EasyTipDialog from './src/EasyTipDialog'
+
+const Dialogs = {
+    EasyTipDialog
+};
+
+module.exports = Dialogs;
---
+++ src/EasyTipDialog.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react'
+import {
+    Text,
+    Modal,
+    View
+} from 'react-native'
+
+export default class EasyTipDialog extends Component{
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <View >
+                <Text>EasyTipDialog</Text>
+            </View>
+        )
+    }
+
+}
